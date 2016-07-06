1557e4ad329ad5a0d22119a482a5139cc0127989
Mitch Paulus <paulusm14@gmail.com>
2016-07-05 20:25:21 -0500

    Removed the .bat files because overleaf wouldn't take them.

--- Plots/DeleteLogFiles.bat
+++
@@ -1,4 +0,0 @@
-REM This is for deleting all the log and aux files that are unecessary for latex.
-forfiles /m *.log /c "cmd /c del @file"
-forfiles /m *.aux /c "cmd /c del @file"
-pause
--- Plots/RebuildPlots.bat
+++
@@ -1,1 +0,0 @@
-forfiles /m *.tex /c "cmd /c lualatex.exe @file"
